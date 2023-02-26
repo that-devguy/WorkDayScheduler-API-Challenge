ff3c4577156c17e6c8dd1c98ece6b81ee8e7e029
Zach Mutch <zkmutch@gmail.com>
2023-02-25 20:54:32 -0500

    added save button click event to save textarea values to parent div and function to load them when the page is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 // Extends API to supply more format options
 dayjs.extend(window.dayjs_plugin_advancedFormat)
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   let currentDayEl = $("#currentDay");
+  //Uses dayjs advanced format to provide the correct date
   let currentDate = dayjs().format("dddd, MMMM Do YYYY");
   let timeBlocks = $(".time-block");
 
   //Displays the current Date in the header of the page
   currentDayEl.text(currentDate);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  //Gets the id of the parent div and the value of the sibling textarea to save the value of the textarea to the current block 
+  $(".saveBtn").on("click", function () {
+    let currentBlock = $(this).parent().attr("id");
+    let eventText = $(this).siblings("textarea").val();
+    localStorage.setItem(currentBlock, eventText);
+  });
 
   //Sets the color of the time-block by checking compairing the current time to the id of the time-block
   function setColor() {
@@ -39,9 +38,15 @@ $(function () {
     });
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  //Loads the textarea values for each timeBlockEl from local storage 
+  function loadEvents() {
+    $(".time-block").each(function () {
+      let currentBlock = $(this).attr("id");
+      let eventText = localStorage.getItem(currentBlock);
+      $(this).find("textarea").val(eventText);
+    });
+  }
 
   setColor();
+  loadEvents();
 });
